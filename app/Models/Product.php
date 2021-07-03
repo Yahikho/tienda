688db5937f2f18a3fe991eeb8bb38c55d0f08594
jhon bermudez pabon <jhonbermudezpa@gmail.com>
2021-07-03 08:50:57 -0500

    Generacion de relaciones Eloquent

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,8 +4,32 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use phpDocumentor\Reflection\DocBlock\Tags\Since;
+use Symfony\Component\VarDumper\Caster\ImgStub;
 
 class Product extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id', 'created_at', 'updated_up'];
+
+    public function brand(){
+        return $this->belongsTo(Brand::class);
+    }
+
+    public function sizes(){
+        return $this->hasMany(Size::class);
+    }
+
+    public function subcategory(){
+        return $this->belongsTo(Subcategory::class);
+    }
+
+    public function colors(){
+        return $this->hasMany(Color::class);
+    }
+
+    public function images(){
+        return $this->morphMany(Image::class, 'imageable' );
+    }
 }
--- app/Models/Size.php
+++ app/Models/Size.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Size extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name','product_id'];
+
+    public function product(){
+        return $this->belongsToMany(Product::class);
+    }
+
+    public function colors(){
+        return $this->belongsToMany(Color::class);
+    }
+    
 }
