133028424d1eb2f2f0ba42315a80c3b3c85cb961
jhon bermudez pabon <jhonbermudezpa@gmail.com>
2021-07-10 11:16:36 -0500

    Insertar registros en la tabla colors

--- database/seeders/ColorSeeder.php
+++ database/seeders/ColorSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-
+use App\Models\Color;
 class ColorSeeder extends Seeder
 {
     /**
@@ -13,6 +13,12 @@ class ColorSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $colors = ['white','blue','red','black'];
+
+        foreach ($colors as $color) {
+            Color::create([
+                'name' => $color
+            ]);
+        }
     }
 }
