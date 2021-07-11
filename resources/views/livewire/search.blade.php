a7f3f27e9e43ca4cb6d2ee40a9d1e023e7ac4347
jhon bermudez pabon <jhonbermudezpa@gmail.com>
2021-07-11 12:28:21 -0500

    Agregar carrito

--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -1,3 +1,8 @@
-<div class="flex-1">
+<div class="flex-1 relative">
     <x-jet-input type="text" class="flex w-full" placeholder="¿Qué estas buscando?"/>
+
+    <button class="absolute right-0 top-0 w-12 h-full bg-orange-500 flex items-center justify-center rounded-r-md">
+        <x-search size="40" color="white" />
+    </button>
+    
 </div>
