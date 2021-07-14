c9b495289de7485222a41ea9326e434f6cdbec80
jhon.bermudez <jhonbermudezpa98@gmail.com>
2021-07-14 17:13:19 -0500

    Solucion desface

--- app/Http/Livewire/CategoryProducts.php
+++ app/Http/Livewire/CategoryProducts.php
@@ -8,6 +8,13 @@ class CategoryProducts extends Component
 {
 
     public $category;
+    public $products = [];
+
+    public function loadPosts(){
+        $this->products = $this->category->products;
+
+        $this->emit('glider');
+    }
     
     public function render()
     {
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -61,5 +61,6 @@
                 }
             }
         </script>
+        @stack('script')
     </body>
 </html>
