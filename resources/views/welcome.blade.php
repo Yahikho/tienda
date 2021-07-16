a7350adec75b8a2803a17dd4dd48767dfbd400b0
jhon bermudez pabon <jhonbermudezpa@gmail.com>
2021-07-15 20:39:24 -0500

    Control pakage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,13 @@
 <x-app-layout>
     <div class="container py-8">
-        <section>
-            <h1 class="text-lg uppercase font-semibold text-gray-700">
-                {{ $categories->first()->name }}
-            </h1>
-            @livewire('category-products', ['category' => $categories->first()])
-        </section>
+        @foreach ($collection as $item)
+            <section>
+                <h1 class="text-lg uppercase font-semibold text-gray-700">
+                    {{ $categories->first()->name }}
+                </h1>
+                @livewire('category-products', ['category' => $categories->first()])
+            </section>
+        @endforeach
     </div>
 
     @push('script')
