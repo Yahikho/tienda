985de5b4550281185f77dcf6ef65ab2a997bc275
jhon.bermudez <jhonbermudezpa98@gmail.com>
2021-07-16 17:09:16 -0500

    Incluit plugin de FlexSlider

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,66 +1,82 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-        <link rel="stylesheet" href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/glider-js/1.7.7/glider.min.css" integrity="sha512-YM6sLXVMZqkCspZoZeIPGXrhD9wxlxEF7MzniuvegURqrTGV2xTfqq1v9FJnczH+5OGFl5V78RgHZGaK34ylVg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
-        @livewireStyles
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
-        <!-- Scripts -->
-        <script src="{{ mix('js/app.js') }}" defer></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/glider-js/1.7.7/glider.min.js" integrity="sha512-tHimK/KZS+o34ZpPNOvb/bTHZb6ocWFXCtdGqAlWYUcz+BGHbNbHMKvEHUyFxgJhQcEO87yg5YqaJvyQgAEEtA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    </head>
-    <body class="font-sans antialiased">
-        <x-jet-banner />
+    <link rel="stylesheet" href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}">
 
-        <div class="min-h-screen bg-gray-100">
-            @livewire('navigation')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/glider-js/1.7.7/glider.min.css"
+        integrity="sha512-YM6sLXVMZqkCspZoZeIPGXrhD9wxlxEF7MzniuvegURqrTGV2xTfqq1v9FJnczH+5OGFl5V78RgHZGaK34ylVg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+        
+    <link rel="stylesheet" href=" {{ asset('vendor/FlexSlider/flexslider.css') }}">
+    @livewireStyles
 
-            <!-- Page Heading -->
+    <!-- Scripts -->
+    <script src="{{ mix('js/app.js') }}" defer></script>
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/glider-js/1.7.7/glider.min.js"
+        integrity="sha512-tHimK/KZS+o34ZpPNOvb/bTHZb6ocWFXCtdGqAlWYUcz+BGHbNbHMKvEHUyFxgJhQcEO87yg5YqaJvyQgAEEtA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
-        @stack('modals')
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 
-        @livewireScripts
+    <script src="{{ asset('vendor/FlexSlider/jquery.flexslider-min.js') }}"></script>
+</head>
 
-        <script>
-            function dropdown(){
-                return{
-                    open : false,
-                    show(){
-                        if(this.open){
-                            //Se cierra el menu
-                            this.open = false
-                            document.getElementsByTagName('html')[0].style.overflow = 'auto'
-                        }else{
-                            //Se abre menú
-                            this.open = true
-                            document.getElementsByTagName('html')[0].style.overflow = 'hidden'
-                        }
-                    },
-                    close(){
+<body class="font-sans antialiased">
+    <x-jet-banner />
+
+    <div class="min-h-screen bg-gray-100">
+        @livewire('navigation')
+
+        <!-- Page Heading -->
+
+        <!-- Page Content -->
+        <main>
+            {{ $slot }}
+        </main>
+    </div>
+
+    @stack('modals')
+
+    @livewireScripts
+
+    <script>
+        function dropdown() {
+            return {
+                open: false,
+                show() {
+                    if (this.open) {
+                        //Se cierra el menu
                         this.open = false
                         document.getElementsByTagName('html')[0].style.overflow = 'auto'
+                    } else {
+                        //Se abre menú
+                        this.open = true
+                        document.getElementsByTagName('html')[0].style.overflow = 'hidden'
                     }
+                },
+                close() {
+                    this.open = false
+                    document.getElementsByTagName('html')[0].style.overflow = 'auto'
                 }
             }
-        </script>
-        @stack('script')
-    </body>
+        }
+    </script>
+    @stack('script')
+</body>
+
 </html>
--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,3 +1,27 @@
 <x-app-layout>
-    
+    <div class="container">
+        <div class="grid grid-cols-2 gap-6">
+            <div>
+                <div class="flexslider">
+                    <ul class="slides">
+                        <li data-thumb="slide1-thumb.jpg">
+                            <img src="slide1.jpg" />
+                        </li>
+                        <li data-thumb="slide2-thumb.jpg">
+                            <img src="slide2.jpg" />
+                        </li>
+                        <li data-thumb="slide3-thumb.jpg">
+                            <img src="slide3.jpg" />
+                        </li>
+                        <li data-thumb="slide4-thumb.jpg">
+                            <img src="slide4.jpg" />
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div>
+
+            </div>
+        </div>
+    </div>
 </x-app-layout>
