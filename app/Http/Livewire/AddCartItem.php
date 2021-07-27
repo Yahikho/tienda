00cffdf033df47888b3ac467f9de8f164f83da01
jhon.bermudez <jhonbermudezpa98@gmail.com>
2021-07-27 09:13:18 -0500

    Habilitar y deshabilitar botones

--- app/Http/Livewire/AddCartItem.php
+++ app/Http/Livewire/AddCartItem.php
@@ -6,8 +6,14 @@ use Livewire\Component;
 
 class AddCartItem extends Component
 {
+    public $product, $quantity;
 
     public $qty = 1;
+
+
+    public function mount(){
+        $this->quantity = $this->product->quantity;
+    }
 
 
     public function decrement(){
