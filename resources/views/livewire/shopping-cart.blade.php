d7ff36d96bb0bbdd014543e04a4ea466f84b7b4d
jhon bermudez pabon <jhonbermudezpa@gmail.com>
2021-07-29 21:42:16 -0500

    Eliminar producto

--- resources/views/livewire/shopping-cart.blade.php
+++ resources/views/livewire/shopping-cart.blade.php
@@ -1,63 +1,106 @@
 <div class="container py-8">
     <section class="bg-white rounded-lg shadow-lg p-6 text-gray-700">
         <h1 class="text-lg font-semibold mb-6">CARRO DE COMPRAS</h1>
-        <table class="table-auto w-full">
-            <thead>
-                <tr>
-                    <th></th>
-                    <th>Precio</th>
-                    <th>Cantidad</th>
-                    <th>Total</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach (Cart::content() as $item)
-                    <tr>
-                        <td>
-                            <div class="flex">
-                                <img class="h-15 w-20 object-cover mr-4" src="{{$item->options->image}}" alt="">
-                                <div>
-                                    <p class="font-bold">{{$item->name}}</p>
-                                    @if ($item->options->color)
-                                        <span>
-                                            Color: {{__($item->options->color)}}
-                                        </span>
-                                    @endif
+        @if (Cart::count())
 
+            <table class="table-auto w-full">
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Precio</th>
+                        <th>Cantidad</th>
+                        <th>Total</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach (Cart::content() as $item)
+                        <tr>
+                            <td>
+                                <div class="flex">
+                                    <img class="h-15 w-20 object-cover mr-4" src="{{ $item->options->image }}" alt="">
+                                    <div>
+                                        <p class="font-bold">{{ $item->name }}</p>
+                                        @if ($item->options->color)
+                                            <span>
+                                                Color: {{ __($item->options->color) }}
+                                            </span>
+                                        @endif
+
+                                        @if ($item->options->size)
+                                            <span class="mx-1">-</span>
+                                            <span>
+                                                {{ __($item->options->size) }}
+                                            </span>
+                                        @endif
+                                    </div>
+                                </div>
+                            </td>
+                            <td class="text-center">
+                                <span>USD {{ $item->price }}</span>
+                                <a class="ml-6 cursor-pointer hover:text-red-600"
+                                    wire:click="delete('{{$item->rowId}}')"
+                                    wire:loading.class="text-red-600 opacity-25"
+                                    wire:target="delete('{{$item->rowId}}')">
+                                    <i class="fas fa-trash"></i>
+                                </a>
+                            </td>
+                            <td>
+                                <div class="flex justify-center">
                                     @if ($item->options->size)
-                                    <span class="mx-1">-</span>
-                                    <span >
-                                        {{__($item->options->size)}}
-                                    </span>
+
+                                        @livewire('update-cart-item-size', ['rowId' => $item->rowId], key($item->rowId))
+
+                                    @elseif($item->options->color)
+
+                                        @livewire('update-cart-item-color', ['rowId' => $item->rowId],
+                                        key($item->rowId))
+
+                                    @else
+
+                                        @livewire('update-cart-item', ['rowId' => $item->rowId], key($item->rowI))
+
                                     @endif
                                 </div>
-                            </div>
-                        </td>
-                        <td>
-                            <span>USD {{$item->price}}</span>
-                            <a class="ml-6 cursor-pointer hover:text-red-600">
-                                <i class="fas fa-trash"></i>
-                            </a>
-                        </td>
-                        <td>
-                            @if ($item->options->size)
+                            </td>
+                            <td class="text-center">
+                                USD {{ $item->price * $item->qty }}
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
 
-                                @livewire('update-cart-item-size', ['rowId' => $item->rowId], key($item->rowI))
+            <a class="text-sm cursor-pointer hover:underline inline-block mt-3" wire:click="destroy">
+                <i class="fas fa-trash"></i>
+                Borrar carrito de compras
+            </a>
+        @else
+            <div class="flex flex-col items-center">
+                <x-cart/>
+                <p class="text-lg text-gray-700 mt-4">TU CARRO DE COMPRAS ESTÁ VACIÓ</p>
 
-                            @elseif($item->options->color)
-
-                                @livewire('update-cart-item-color', ['rowId' => $item->rowId], key($item->rowI))
-
-                            @else
-
-                                @livewire('update-cart-item', ['rowId' => $item->rowId], key($item->rowI))
-                                
-                            @endif
-                        </td>
-                        <td></td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
+                <a href="/" class="mt-4">
+                    <x-button>
+                        Ir al inicio
+                    </x-button>
+                </a>
+            </div>
+        @endif
     </section>
+    @if (Cart::count())
+        <div class="bg-white rounded-lg shadow-lg px-6 py-4 mt-4">
+            <div class="flex justify-between items-center">
+                <div>
+                    <p class="text-gray-700">
+                        <span class="font-bold text-lg">Total: </span> {{Cart::subTotal()}}
+                    </p>
+                </div>
+                <div>
+                    <x-button-enlace color="orange">
+                        CONTINUAR
+                    </x-button-enlace>
+                </div>
+            </div>
+        </div>
+    @endif
 </div>
